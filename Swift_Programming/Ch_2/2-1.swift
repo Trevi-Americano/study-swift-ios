4d594e2b59d5fbbe68a61c5ba8a99c0efaa0d2b2
Hae Chang Lee <dlgockd45@gmail.com>
2020-04-06 21:37:13 +0900

    Show what is string interpolation

---
+++ Swift_Programming/Ch_2/2-1.swift
@@ -0,0 +1,8 @@
+// 문자열 보간법(String Interpolation)
+// 변수 또는 상수 등의 값을 문자열 내에 나타내고 싶을 때 사용
+// 문자열 내에 \(변수 나 상수) 형태로 표기
+// String interpolation을 이용해서 문자열로 치환하려면 변수나 상수 타입을
+// CustomStringConvertible 프로토콜을 준수하는 프로퍼티로 구현
+
+let name = "hclee"
+print("My name is \(name)")
