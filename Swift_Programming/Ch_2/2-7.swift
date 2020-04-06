9cbf0b2f0182ea390e99d71b067ffe79abc1fab4
Hae Chang Lee <dlgockd45@gmail.com>
2020-04-06 21:59:58 +0900

    Show how to use constant

---
+++ Swift_Programming/Ch_2/2-7.swift
@@ -0,0 +1,9 @@
+let name = "hclee"
+var age = 29
+var job = "Android Developer"
+
+print("\(type(of: job))")
+print("저의 이름은 \(name)이고 나이는 \(age)세이며 직업은 \(job)입니다.")
+
+name = "hclee2" // Swift에서 상수는 let 키워드를 사용해서 선언한다.
+print("저의 이름은 \(name)입니다.")
