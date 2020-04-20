bbb3af78923024962e197f76962f6aa2170712ff
Hae Chang Lee <dlgockd45@gmail.com>
2020-04-20 23:07:40 +0900

    [I Am Rich] Add Xcode project files

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Hae Chang Lee on 4/20/20.
+//  Copyright © 2020 Hae Chang Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
