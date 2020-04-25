754af082559aef0fdbd8eb85ec9d8516d0b15ef2
Hae Chang Lee <dlgockd45@gmail.com>
2020-04-25 19:13:41 +0900

    [I Am Poor] Add Xcode project files

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Hae Chang Lee on 4/25/20.
+//  Copyright © 2020 Hae Chang Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
